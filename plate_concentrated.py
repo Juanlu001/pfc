34f6e6e472c215fd75b5e59327f460720886e0eb
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2015-02-06 12:32:40 +0100

    Added SquarePlateDisplacement class Now all the computations take place inside the class so the user only has to provide the geometry, the material properties and the load conditions.

--- plate_concentrated.py
+++ plate_concentrated.py
@@ -16,15 +16,12 @@ References
 
 TODO
 ----
-* Don't hardcode array sizes
 * Compute global and local error with respect to mixed biharmonic solution
 * Make plots of the above
 * Study number of points and interpolation degree
 * Check several alpha values from Timoshenko
 * Measure performance
-* Separate in modules
 * Extend for other load conditions
-* Don't import all FEniCS
 
 """
 from __future__ import division
@@ -34,6 +31,12 @@ from numpy import pi, sin
 from scipy import interpolate
 
 from numba import njit
+
+from dolfin.cpp import mesh as meshes
+from dolfin.functions import functionspace, expression
+from dolfin.fem import interpolation
+from dolfin.common import plotting
+
 
 # If no type hinting is provided, it is inferred in the first call
 @njit
@@ -51,8 +54,10 @@ def plate_displacement(xx, yy, ww, a, b, P, xi, eta, D, max_m, max_n):
     """
     # Unroll all the loops for glorious numba efficiency
     max_i, max_j = ww.shape
+    # Sum x and y series terms
     for mm in range(1, max_m):
         for nn in range(1, max_n):
+            # Sum for every point
             for ii in range(max_i):
                 for jj in range(max_j):
                     a_mn = a_mn_point(P, a, b, xi, eta, mm, nn)
@@ -62,9 +67,68 @@ def plate_displacement(xx, yy, ww, a, b, P, xi, eta, D, max_m, max_n):
                                    / (pi**4 * D))
 
 
-if __name__ == '__main__':
-    # --- Initial data
+class SquarePlateDisplacement(expression.Expression):
+    """Transverse displacement of a simply supported rectangular plate.
 
+    This expression represents the exact Navier solution.
+
+    Notes
+    -----
+    The Navier solution is computed internally using jit-compiled functions
+    for higher performance. The result is then interpolated using cubic
+    splines, so it is not mandatory to specify a big number of points
+    to obtain good precision.
+
+    """
+    def __init__(self, mesh, h, E, nu, P, xi, eta,
+                 num_points=11, max_m=64, max_n=64):
+        """Constructor.
+
+        Parameters
+        ----------
+        mesh : mesh.RectangleMesh
+            Mesh corresponding to the plate geometry.
+        h : float
+            Thickness of the plate.
+        E : float
+            Young modulus [Pa].
+        nu : float
+            Poisson's ratio.
+        P : float
+            Point load [N].
+        xi, eta : float
+            Coordinates of the load application point [m].
+        num_points : int, optional
+            Number of points for the intermediate meshgrid, defaults to 11.
+        max_m, max_n : int, optional
+            Maximum number of series terms to sum, default to 64.
+
+        """
+        # Geometry
+        assert isinstance(mesh, meshes.RectangleMesh)
+        coords = mesh.coordinates()
+        a, b = coords.max(axis=0) - coords.min(axis=0)
+
+        # Material
+        D = h**3 * E / (12 * (1 - nu**2))
+
+        # Create domain
+        x = np.linspace(0, a, num=num_points)
+        y = np.linspace(0, b, num=num_points)
+        xx, yy = np.meshgrid(x, y)
+
+        # Compute displacement
+        ww = np.zeros_like(xx)
+        plate_displacement(xx, yy, ww, a, b, P, xi, eta, D, max_m, max_n)
+
+        # Interpolate
+        self._w = interpolate.RectBivariateSpline(x, y, ww, kx=3, ky=3)
+
+    def eval(self, value, x):
+        value[0] = self._w(x[0], x[1])
+
+
+if __name__ == '__main__':
     # Plate geometry
     a = 1.0  # m
     b = 1.0  # m
@@ -74,55 +138,25 @@ if __name__ == '__main__':
     E = 69e9  # Pa
     nu = 0.35
 
-    # Series terms
-    max_m = 64
-    max_n = 64
-
-    # Computation points
-    # NOTE: With an odd number of points the center of the place is included in
-    # the grid
-    NUM_POINTS = 11
-
     # Load
     P = -10e3  # N
     xi = a / 2
-    eta = a / 2
+    eta = b / 2
 
     # Flexural rigidity
     D = h**3 * E / (12 * (1 - nu**2))
 
-    # ---
+    # Now comes the FEniCS magic
+    mesh = meshes.RectangleMesh(0, 0, a, b, 100, 100)
+    V = functionspace.FunctionSpace(mesh, 'Lagrange', 1)
 
-    # Set up domain
-    x = np.linspace(0, a, num=NUM_POINTS)
-    y = np.linspace(0, b, num=NUM_POINTS)
-    xx, yy = np.meshgrid(x, y)
-
-    # Compute displacement field
-    ww = np.zeros_like(xx)
-    plate_displacement(xx, yy, ww, a, b, P, xi, eta, D, max_m, max_n)
-
-    # Don't use RegularGridInterpolator as it is intended for arbitrary dimensions
-    # and doesn't support quadratic or cubic interpolation
-    w = interpolate.RectBivariateSpline(x, y, ww, kx=3, ky=3)
+    u_expr = SquarePlateDisplacement(mesh, h, E, nu, P, xi, eta)
+    u = interpolation.interpolate(u_expr, V)
 
     # Print maximum displacement
-    w_max = abs(ww).max()
-    print "Displacement at the center = %14.12f mm" % (w(a / 2, b / 2) * 1e3)
-    print "Maximum displacement (abs) = %14.12f mm" % (w_max * 1e3)
+    w_max = np.abs(u.vector().array()).max()
+    print "Maximum displacement = %14.12f mm" % (w_max * 1e3)
     print "alpha = %7.5f" % (w_max / (P * a**2 / D))
     print "alpha * P a^2 / D = %6.4f mm" % (0.01160 * P * a**2 / D * 1e3)
 
-    # Now comes the FEniCS magic
-    from fenics import *
-
-    mesh = UnitSquareMesh(100, 100)
-    V = FunctionSpace(mesh, 'Lagrange', 1)
-
-    class SquarePlate(Expression):
-        def eval(self, value, x):
-            value[0] = w(x[0], x[1])
-
-    u = interpolate(SquarePlate(), V)
-
-    plot(u, interactive=True)
+    plotting.plot(u, interactive=True)
